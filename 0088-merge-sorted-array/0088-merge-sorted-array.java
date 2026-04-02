b54d1a95829e04dee6edf29df0192b0d87461788
Somesh Namta <somesh.namata32@gmail.com>
2026-04-03 01:15:36 +0530

    Time: 4 ms (15.1%), Space: 44.2 MB (10.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,15 +3,17 @@ class Solution {
         for(int i=0; i<n; i++){
             nums1[m+i]=nums2[i];
         }
+
+        Arrays.sort(nums1);
       
-        for(int i=0; i<m+n-1; i++){
-            for(int j=0; j<m+n-1-i; j++){
-            if(nums1[j]>nums1[j+1]){
-                int temp = nums1[j];
-                nums1[j] = nums1[j+1];
-                nums1[j+1]=temp;
-            }
-        }
-    }
+        //     for(int i=0; i<m+n-1; i++){
+        //         for(int j=0; j<m+n-1-i; j++){
+        //         if(nums1[j]>nums1[j+1]){
+        //             int temp = nums1[j];
+        //             nums1[j] = nums1[j+1];
+        //             nums1[j+1]=temp;
+        //         }
+        //     }
+        // }
     }
 }
