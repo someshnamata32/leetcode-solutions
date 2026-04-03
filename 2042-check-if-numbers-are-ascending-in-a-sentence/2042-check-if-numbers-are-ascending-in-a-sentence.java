7a7cdda8992c2b646fefcece11d4fd9cf2839ee0
Somesh Namta <somesh.namata32@gmail.com>
2026-04-04 00:43:13 +0530

    Time: 1 ms (86.76%), Space: 42.4 MB (97.53%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean areNumbersAscending(String s) {
+        int num = 0;
+        int prev = 0;
+        for(int i=0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+               num = num * 10 + (s.charAt(i) - '0');
+            }
+            else if(num != 0){
+                if(num <= prev) return false;
+                prev = num;
+                num =0;
+            }
+        }
+            if(num != 0 && num <= prev) return false;
+        return true;
+    }
+}
