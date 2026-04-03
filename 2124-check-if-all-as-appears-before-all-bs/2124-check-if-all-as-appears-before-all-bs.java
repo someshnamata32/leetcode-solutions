95c2f17899b502dd1062b8a561e6c95542dcb1f9
Somesh Namta <somesh.namata32@gmail.com>
2026-04-04 00:55:40 +0530

    Time: 0 ms (100%), Space: 43 MB (37.13%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean checkString(String s) {
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i) == 'a' && s.charAt(i-1) == 'b') return false;
-        }
+        for(int i=1; i<s.length(); i++) 
+            if(s.charAt(i) == 'a' && s.charAt(i-1) == 'b') return false;  
         return true;
     }
 }
