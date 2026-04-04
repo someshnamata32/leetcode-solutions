5659ef50c7d04f8d3ac1e21b1c53f0ad638ef4c3
Somesh Namta <somesh.namata32@gmail.com>
2026-04-05 01:46:54 +0530

    Time: 7 ms (65.35%), Space: 44.5 MB (44.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -5,11 +5,7 @@ class Solution {
         
         for(int i= str.length-1 ; i>=0; i--){
             result.append(str[i]);
-             if(i != 0){
-               
-                result.append(" ");
-             }
-
+            if(i != 0) result.append(" ");
         }
         return result.toString();
     }
