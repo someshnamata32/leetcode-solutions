5dfe4103d31c63b6ee4e0951cfd0237f4509a489
Somesh Namta <somesh.namata32@gmail.com>
2026-04-19 16:56:22 +0530

    Time: 55 ms (5.02%), Space: 42.9 MB (11.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+       // return (int)Math.sqrt(x);
+
+        int ans=0;
+       for(int i =1; i<=x; i++){
+        if(i <= x/i) ans = i;
+        else break;
+       }
+       return ans;
     }
 }
