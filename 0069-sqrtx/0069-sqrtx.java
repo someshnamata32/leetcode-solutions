095fc30f770812756adc27609ca280524e95405e
Somesh Namta <somesh.namata32@gmail.com>
2026-04-19 17:49:32 +0530

    Time: 1 ms (97.8%), Space: 42.8 MB (11.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,11 +2,22 @@ class Solution {
     public int mySqrt(int x) {
        // return (int)Math.sqrt(x);
 
-        int ans=0;
-       for(int i =1; i<=x; i++){
-        if(i <= x/i) ans = i;
-        else break;
-       }
-       return ans;
+    //     int ans=0;
+    //    for(int i =1; i<=x; i++){
+    //     if(i <= x/i) ans = i;
+    //     else break;
+    //    }
+    //    return ans;
+
+    int left=1,right=x,ans=0;
+    if(x<2) return x;
+    while(left <= right){
+        int mid = left +(right-left)/2;
+        if(mid <= x/mid) {
+            ans = mid; left = mid+1;
+        }else right = mid-1;
+    }
+    return right;
+
     }
 }
