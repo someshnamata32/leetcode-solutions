b745e2b596d1c8ab47906a97d170e6aa52339196
Somesh Namta <somesh.namata32@gmail.com>
2026-06-05 19:37:16 +0530

    Time: 2 ms (99.15%), Space: 44.2 MB (83.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,50 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         String str = "";
+
+//         for(int i = 0; i<s.length(); i++){
+//             char ch = s.charAt(i);
+//             if(Character.isLetterOrDigit(ch))
+//             str += Character.toLowerCase(ch);
+//         }
+
+//         int l=0;
+//         int r = str.length()-1;
+//         while(l<r){
+//             if(str.charAt(l)!=str.charAt(r)) return false;
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = "";
 
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch))
-            str += Character.toLowerCase(ch);
-        }
+        int l = 0;
+        int r = s.length() - 1;
 
-        int l=0;
-        int r = str.length()-1;
-        while(l<r){
-            if(str.charAt(l)!=str.charAt(r)) return false;
+        while (l < r) {
+
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+
+            if (Character.toLowerCase(s.charAt(l))
+                != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+
             l++;
             r--;
         }
+
         return true;
     }
 }
