cb40c230576ce89e2dcf102b9589b11f0d75ad08
Somesh Namta <somesh.namata32@gmail.com>
2026-06-06 19:36:57 +0530

    Time: 1 ms (98.43%), Space: 47.4 MB (58.13%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int n:nums) total+=n;
+        int lsum=0,rsum=0;
+        for(int i=0; i<nums.length; i++){
+            rsum = total - lsum - nums[i];
+            if(lsum == rsum) return i;
+            lsum+=nums[i];
+        }
+        return -1;
+    }
+}
