9a638e09198c1fb8bcef4b5cdf586518afbc1db8
Somesh Namta <somesh.namata32@gmail.com>
2026-06-19 02:06:51 +0530

    Time: 2 ms (95.16%), Space: 47.6 MB (64.97%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Set<Integer> set = new HashSet<>();
         int duplicate = -1;
         int missing  = -1;
-        for(int n : nums)
-            if(set.contains(n)) duplicate = n;
-            else set.add(n);
+        int n = nums.length;
+        int[] freq = new int[n+1];
 
-            for(int i=1; i<=nums.length; i++){
-                if(!set.contains(i)) {
-                    missing = i;
-                    break;
-                }
-            }
+        for(int i=0; i<n; i++) freq[nums[i]]++;
+
+        for(int i=1; i<=nums.length; i++){
+                if(freq[i] == 2) duplicate = i;
+                if(freq[i] == 0) missing = i;
+        }
             return new int[]{duplicate,missing};
     }
 }
