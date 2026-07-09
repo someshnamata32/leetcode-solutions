2a4cc72006d5dec24fa741616bbaedfd1cb6c072
Somesh Namta <somesh.namata32@gmail.com>
2026-07-09 15:13:21 +0530

    Time: 8 ms (36.75%), Space: 49.4 MB (92.31%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String , List<String>> map = new HashMap<>();
+        for(String word : strs){
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            if(!map.containsKey(key))
+                map.put(key,new ArrayList<>());
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
