796b5f11c5fc6f27b71a6ed95f564ca26f840376
Somesh Namta <somesh.namata32@gmail.com>
2026-07-19 05:18:15 +0530

    Time: 15 ms (61.88%), Space: 48.3 MB (68.88%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
+                stack.pop();
+            }
+
+            if (i < n && !stack.isEmpty()) {
+                ans[i] = stack.peek();
+            }
+
+            stack.push(nums[i % n]);
+        }
+
+        return ans;
+    }
+}
