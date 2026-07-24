659a1cf016ec8c444751c812b678c1eb4eb03146
Somesh Namta <somesh.namata32@gmail.com>
2026-07-25 01:00:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,26 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2) return n;
-        int f=1;
-        int s=2;
-        for(int i=3; i<=n; i++){
-            int t = f + s;
-            f=s;
-            s=t;
-        }
-        return s;
+        // if(n<=2) return n;
+        // int f=1;
+        // int s=2;
+        // for(int i=3; i<=n; i++){
+        //     int t = f + s;
+        //     f=s;
+        //     s=t;
+        // }
+        // return s;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp,-1);
+        return helper(n, dp);
+
+    }
+
+    int helper(int n, int[] dp) {
+        if (n == 1 || n == 2)
+            return n;
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
 }
