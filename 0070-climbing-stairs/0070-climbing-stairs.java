a562363a88c70b74a666b189ce804024713bc34a
Somesh Namta <somesh.namata32@gmail.com>
2026-07-25 10:05:12 +0530

    Time: 0 ms (100%), Space: 42 MB (54.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,17 +10,33 @@ class Solution {
         // }
         // return s;
 
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp,-1);
-        return helper(n, dp);
 
-    }
+        // memoization
+    //     int[] dp = new int[n + 1];
+    //     Arrays.fill(dp, -1);
+    //     return helper(n, dp);
 
-    int helper(int n, int[] dp) {
-        if (n == 1 || n == 2)
-            return n;
-        if (dp[n] != -1)
-            return dp[n];
-        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+    // }
+
+    // int helper(int n, int[] dp) {
+    //     if (n == 1 || n == 2)
+    //         return n;
+    //     if (dp[n] != -1)
+    //         return dp[n];
+    //     return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+
+
+    //tabulation
+
+    int[] dp = new int[n+1];
+    if(n==1 || n==2) 
+        return n;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i=3; i<=n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+
+    return dp[n]; 
     }
 }
