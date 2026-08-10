76b10e166026c78df6e73fead345c02e35a0d5cb
Somesh Namta <somesh.namata32@gmail.com>
2026-08-10 21:42:21 +0530

    Time: 3 ms (69.82%), Space: 53.5 MB (68.43%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,19 +1,31 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
 
-        int[] indegree = new int[n + 1];
-        int[] outdegree = new int[n + 1];
+        // int[] indegree = new int[n + 1];
+        // int[] outdegree = new int[n + 1];
 
-        for (int i = 0; i < trust.length; i++) {
-            int u = trust[i][0];
-            int v = trust[i][1];
+        // for (int i = 0; i < trust.length; i++) {
+        //     int u = trust[i][0];
+        //     int v = trust[i][1];
 
-            outdegree[u]++;
-            indegree[v]++;
+        //     outdegree[u]++;
+        //     indegree[v]++;
+        // }
+
+        // for (int i = 1; i <= n; i++)
+        //     if (indegree[i] == n - 1 && outdegree[i] == 0)
+        //         return i;
+
+        // return -1;
+
+        int[] score = new int[n + 1];
+        for (int[] t : trust) {
+            score[t[0]]--;
+            score[t[1]]++;
         }
 
         for (int i = 1; i <= n; i++)
-            if (indegree[i] == n - 1 && outdegree[i] == 0)
+            if (score[i] == n - 1)
                 return i;
 
         return -1;
