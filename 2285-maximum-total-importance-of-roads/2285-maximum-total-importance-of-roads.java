dfd7d62ee68974d58b93c46cb6af44369cd7eaa9
Somesh Namta <somesh.namata32@gmail.com>
2026-08-11 19:20:10 +0530

    Time: 13 ms (61.47%), Space: 117.2 MB (12.97%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maximumImportance(int n, int[][] roads) {
+
+        int[] degree = new int[n];
+
+        for (int[] road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
+        Arrays.sort(degree);
+
+        long ans = 0;
+        for (int i = 0; i < n; i++)
+            ans += (long) degree[i] * (i + 1);
+
+        return ans;
+    }
+}
