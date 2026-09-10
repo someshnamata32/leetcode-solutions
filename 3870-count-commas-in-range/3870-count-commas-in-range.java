fa1b45cd1ba1b760d918671386a54e2d8990a07d
Somesh Namta <somesh.namata32@gmail.com>
2026-09-10 17:22:21 +0530

    Time: 1 ms (85.9%), Space: 42.7 MB (20.8%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,6 +1,10 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000) return 0;
-        return n-1000+1;
+        if(n<1000){
+            return 0;
+        }
+        else{
+            return n-999;
+        }
     }
 }
